fd79e84f775fe8daaf9a0fe91ffbcea61ccf71d6
unknown <joel.bustos@gmail.com>
2024-02-09 21:50:21 -0300

    Protecto final con detalles

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -9,20 +9,16 @@ const ItemDetailContainer = () => {
     const { id } = useParams()
 
     useEffect(() => {
-        if (producto) document.title = producto.title ?? 'Nona Delma'
-        else document.title = 'Nona Delma'
-    }, [producto])
-
-    useEffect(() => {
         const productDocument = doc(db, 'productos-nonadelma', id)
         getDoc(productDocument)
             .then(queryDocumentSnapshot => {
                 if (queryDocumentSnapshot.exists()) {
                     const fields = queryDocumentSnapshot.data()
-                    const productAdapted = { id: queryDocumentSnapshot.id, ...fields}
+                    const productAdapted = { id: queryDocumentSnapshot.id, ...fields }
                     setProducto(productAdapted)
                 } else {
                     console.error(`El producto con ID ${id} no existe.`)
+                    setProducto({})
                 }
             })
             .catch(error => {
@@ -30,8 +26,12 @@ const ItemDetailContainer = () => {
             })
     }, [id])
 
-    if (!producto) {
+    if (producto === null) {
         return <p className='cargando'>Cargando...</p>
+    }
+
+    if (Object.keys(producto).length === 0) {
+        return <p className='cargando'>Producto no encontrado</p>
     }
 
     return (
